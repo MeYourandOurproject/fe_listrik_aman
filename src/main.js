c00bc593085411f693f11f39835f9a1887d4f02d
RULY ABDUL RASYID <rrulyabdul@gmail.com>
2026-03-26 11:49:19 +0700

    api for newartikel at HomePage

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from "./router";
 import store from "./store";
 
 import "bootstrap/dist/css/bootstrap.css";
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
 // createApp(App).use(bootstrap).use(router).mount("#app");
 
-createApp(App).use(bootstrap).use(router).use(store).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
